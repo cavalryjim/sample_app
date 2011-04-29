414e285d013f43516bfd8395ca09c74e257ed1a0
James Davis <james.davisphd@gmail.com>
2011-04-29 16:54:45 -0500

    Added user following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,10 +4,9 @@ describe PagesController do
   render_views
 
   before(:each) do
-    #
-    # Define @base_title here.
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -42,6 +41,39 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title", :content => @base_title + " | About")
+    end
+    
+    describe "when not signed in" do
+
+      before(:each) do
+        get :home
+      end
+
+      it "should be successful" do
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Home")
+      end
+    end
+
+    describe "when signed in" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+
+      it "should have the right follower/following counts" do
+        get :home
+        response.should have_selector("a", :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector("a", :href => followers_user_path(@user),
+                                           :content => "1 follower")
+      end
     end
   end
 end
